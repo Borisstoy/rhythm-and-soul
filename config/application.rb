97506c74f732f82781c28aa4ec98120caf239670
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-21 10:21:42 +0100

    i18n-setup

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#I18N
+config.i18n.default_locale = :en
+
 module RhythmAndSoul
   class Application < Rails::Application
     config.generators do |generate|
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index, :show] do
-    resources :bookmarks, only: [:index, :show,:create]
+  scope '(:locale)', locale: /fr/ do
+    root to: 'pages#home'
+    resources :events, only: [:index, :show] do
+      resources :bookmarks, only: [:index, :show,:create]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
 end
