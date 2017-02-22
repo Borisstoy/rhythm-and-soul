aeed69bdfdc2655d50ee069dbd9a0a9be4303405
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-22 15:44:43 +0100

    TEST-10-playlists-persisted-artists-in-db-created-user-artist-relation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     # RSpotify.authenticate("SPOTIFY_CLIENT_ID", "SPOTIFY_CLIENT_SECRET")
     # spotify_user = RSpotify::User.find(current_user.spotify_uid)
     # raise
-    @toto = get_artists_names
+    @toto = artists_persistence
   end
 
   private
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
     url = "https://api.spotify.com/v1/users/#{@user.spotify_id}/playlists?limit=50&offset=#{offset}"
     playlists_serialized = RestClient::Resource.new(url, headers: {accept: "application/json", authorization: "Bearer #{@user.auth_token}"})
     @partial_playlist = JSON.parse(playlists_serialized.get)
-    @partial_playlist
   end
 
   def playlists_ids_storing
@@ -50,12 +49,29 @@ class UsersController < ApplicationController
     @playlists_ids
   end
 
-  # def get_artists_names
-  #   playlists_ids_storing.each do |playlist_id|
-  #     url = "https://api.spotify.com/v1/users/#{@user.spotify_id}/playlists/#{playlist_id}/tracks"
-  #     tracks_serialized = RestClient::Resource.new(url, headers: {accept: "application/json", authorization: "Bearer #{@user.auth_token}"})
-  #     @tracks = JSON.parse(tracks_serialized.get)
-  #     p @tracks
-  #   end
-  # end
+  def get_tracks_artists
+    tracks = []
+    playlists_ids_storing[0..9].each do |playlist_id|
+      url = "https://api.spotify.com/v1/users/#{@user.spotify_id}/playlists/#{playlist_id}/tracks"
+      tracks_serialized = RestClient::Resource.new(url, headers: {accept: "application/json", authorization: "Bearer #{@user.auth_token}"})
+      tracks << JSON.parse(tracks_serialized.get)
+    end
+    @artists_names = tracks.map do |track|
+      track["items"][0]["track"]["album"]["artists"][0]["name"]
+    end
+  end
+
+  def artists_persistence
+    get_tracks_artists
+    @artists_names.each do |artist|
+      unless Artist.where(name: artist).exists?
+        new_artist = Artist.new(name: artist)
+        new_artist.save
+        new_user_artist = UserArtist.new()
+        new_user_artist.artist = new_artist
+        new_user_artist.user = @user
+        new_user_artist.save
+      end
+    end
+  end
 end
