f3d67724207b024d1b4078781ed93a8f3fc3842e
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-02-24 11:01:57 +0100

    google fix

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -16,6 +16,10 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  units: :km,                 # :km for kilometers or :mi for miles
+  # units: :mi,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
+  :units => :km,
+  :lookup    => :google,
+  :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+  :use_https => true
 )
