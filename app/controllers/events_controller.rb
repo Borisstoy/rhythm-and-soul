189c5986ce9ac12723a184fea931d8405b0a6546
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-03-22 13:47:22 -0400

    deleted l.89

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -86,7 +86,7 @@ center_map_display(@location)
         <h3 class='iw-title'>#{event.venue.name}</h3>
         <div class='iw-event'>
           <div>
-            <h3><%= #{event}.blank? ? 'N/A' : #{event.artists.first.name} %></h3>
+
             <div>
               #{event.date.strftime('%d %b %Y')}
             </div>
