6ca0a870d080ec2ff8ee1a1d3cd3a9d92e2b6f38
Boris Stoyanov-Brignoli <boris.stoy@gmail.com>
2017-03-30 08:57:29 -0400

    fixing scanning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_locale
-  before_action :check_for_scanning
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || events_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || user_path
   end
 
   def default_url_options
@@ -21,8 +20,4 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def check_for_scanning
-    @scanning = session[:scanning]
-    session[:scanning] = nil # reset scanning flag
-  end
 end
